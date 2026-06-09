4f632f72c75138e6eb1a41a17810f3ab7420f84c
Elias Gabriel <eliasgabrielcf@gmail.com>
2026-06-09 17:11:33 -0300

    Layouts, CSS, sessões. dotenv

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,10 @@
+require "dotenv/load"
 require "sinatra"
 
 # Sinatra configs
 set :views, File.expand_path("../views", __dir__)
+set :public_folder, File.join(__dir__, "public")
 set :show_exceptions, false # não "explodir" quando tiver exceção
 set :dump_errors, false     # omita a stacktrace
+set :session_secret, ENV.fetch("SESSION_SECRET")
 enable :sessions
